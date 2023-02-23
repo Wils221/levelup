a86e3db7c169d1d4dba9af6632759c379ee33718
Wil Hammond <wil.hammond@gmail.com>
2023-02-23 15:33:31 -0600

    Final

--- levelupapi/views/event_view.py
+++ levelupapi/views/event_view.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import serializers, status
 from levelupapi.models import Event
 from levelupapi.models import Gamer, Game, gamer
+from rest_framework.decorators import action
 
 
 class EventView(ViewSet):
@@ -78,6 +79,22 @@ class EventView(ViewSet):
         event.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 
+    @action(methods=['post'], detail=True)
+    def signup(self, request, pk):
+        gamer = Gamer.objects.get(user=request.auth.user)
+        event = Event.objects.get(pk=pk)
+        event.attendees.add(gamer)
+        return Response({'message': 'Gamer added'}, status=status.HTTP_201_CREATED)
+
+    @action(methods=['delete'], detail=True)
+    def leave(self, request, pk):
+        gamer = Gamer.objects.get(user=request.auth.user)
+        event = Event.objects.get(pk=pk)
+        event.attendees.remove(gamer)
+        return Response({'message': 'Gamer left'}, status=status.HTTP_204_NO_CONTENT)
+    
+
+
 class GamerSerializer(serializers.ModelSerializer):
     """JSON serializer for event
     """
@@ -91,5 +108,5 @@ class EventSerializer(serializers.ModelSerializer):
     organizer = GamerSerializer()
     class Meta:
         model = Event
-        fields = ('id', 'organizer', 'name', 'date', 'location', 'game' )
+        fields = ('id', 'name', 'game', 'location', 'organizer', 'date', 'attendees','joined')
         depth = 1
